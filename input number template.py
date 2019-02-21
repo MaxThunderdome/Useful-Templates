e35e7db86c196eeff9f9d1b85d80b1a656d8cf83
MaxThunderdome <47267220+MaxThunderdome@users.noreply.github.com>
2019-02-21 13:50:34 -0800

    Input template - both append values and add integers

---
+++ input number template.py
@@ -0,0 +1,10 @@
+#to append the results
+x = input("Please Enter a Number")
+y = input("Please Enter another number")
+result = x + y
+print(result)
+#int() to complete addition
+a = int(input("Please Enter a Number"))
+b = int(input("Please Enter another number"))
+result = a +b
+print(result)
